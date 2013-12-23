04321491d569ea0cbce08ae0fa41f5e2dba12d16
Paolo Bosetti <paolo.bosetti@unitn.it>
2013-12-23 18:02:40 +0100

    Updarted build script

--- src/build.bat
+++ src/build.bat
@@ -1,10 +1,5 @@
-rem cl /MD /O2 /W3 /c /DLUA_BUILD_AS_DLL /D_CRT_SECURE_NO_WARNINGS *.c
-cl /MD /O2 /W3 /c /D_CRT_SECURE_NO_WARNINGS *.c
-del *.o
-ren lua.obj lua.o
-ren luac.obj luac.o
-ren print.obj print.o
-rem link /OUT:lua5.2.lib *.obj
-rem link /OUT:lua.exe lua.o lua5.2.lib
+rem cl /O2 /W3 /Zl /c /DLUA_BUILD_AS_DLL /D_CRT_SECURE_NO_WARNINGS *.c
+cl /c /nologo /Zi /W3 /WX- /EHa /GS /Od /MD /D_CRT_SECURE_NO_WARNINGS *.c
+del lua.obj luac.obj print.obj
 lib /out:lua5.2-static.lib *.obj
 rem link /OUT:luac.exe luac.o print.o lua5.2-static.lib
