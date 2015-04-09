0c0f76d00c4efa42edfe5840c48dbef0ee3be9bb
Paolo Bosetti <paolo.bosetti@unitn.it>
2015-04-09 11:59:32 +0200

    Windows build script updated for lua version 5.3

--- src/build.bat
+++ src/build.bat
@@ -1,5 +1,5 @@
 rem cl /O2 /W3 /Zl /c /DLUA_BUILD_AS_DLL /D_CRT_SECURE_NO_WARNINGS *.c
 cl /c /nologo /Zi /W3 /WX- /EHa /GS /Od /MD /D_CRT_SECURE_NO_WARNINGS *.c
 del lua.obj luac.obj print.obj
-lib /out:lua5.2-static.lib *.obj
+lib /out:lua-static.lib *.obj
 rem link /OUT:luac.exe luac.o print.o lua5.2-static.lib
